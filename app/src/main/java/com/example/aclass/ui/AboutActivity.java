8c382a90b3c7bdbe160eb81e3737cf4c8abeb092
upupming <upupming@gmail.com>
2018-09-26 21:46:02 +0800

    Fixed: github link

--- app/src/main/java/com/example/aclass/ui/AboutActivity.java
+++ app/src/main/java/com/example/aclass/ui/AboutActivity.java
@@ -45,7 +45,7 @@ public class AboutActivity extends AppCompatActivity {
                 .setDescription("本软件仅供哈工大本科生进行课表相关操作使用，项目已开源。如果你在使用的过程中发现任何bug，或对本软件的发展有建议，请联系yuxiang.wei.cs@gmail.com。感谢您的支持。")
                 .setImage(R.drawable.bg_black)
                 .addItem(help)
-                .addGitHub("https://github.com/Yuxiang-Wei/HITSchedule")
+                .addGitHub("Yuxiang-Wei/HITSchedule")
                 .addEmail("Yuxiang.Wei.CS@gmail.com")
                 .addGroup("Thanks for")
                 .addItem(timetableView)
