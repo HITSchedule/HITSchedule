35a616ac6d5bb0da834cc086fffec384bab0cf5f
leylee <leylee2001@gmail.com>
2020-08-12 23:08:57 +0800

    add test unit

--- app/src/test/java/com/example/hitschedule/ExampleUnitTest.java
+++ app/src/test/java/com/example/hitschedule/ExampleUnitTest.java
@@ -1,6 +1,10 @@
 package com.example.hitschedule;
 
+import com.example.hitschedule.util.HttpUtil;
+
 import org.junit.Test;
+
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +17,17 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void wechatPostTest() throws IOException {
+        for (int day = 1; day <= 7; ++day) {
+            String json = HttpUtil.wechatBksKbPost("1190501001", "2019-2020;2", day);
+            if (json == null) {
+                System.out.println("error on day" + day);
+            } else {
+                System.out.println(json);
+            }
+        }
     }
 }
